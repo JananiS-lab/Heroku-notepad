c4d2e48698813dce5604f852db764978aabbdbfa
ramabhadran kapistalam <jananis.sekar@gmail.com>
2020-06-27 17:03:13 -0400

    add psycopg2

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import sqlalchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "sqlite:///notepad.sqlite" 
 
+db = sqlalchemy(app)
+
+class Duties(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+
+    
 @app.route('/')
 def index():
     return render_template('index.html', name="Janani")
